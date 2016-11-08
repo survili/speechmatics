fdf0e1956d318cf85c92031d55ae1f71fafe6954
Kobi Ben Tzvi <tsumbush@gmail.com>
2016-11-08 17:12:15 +0200

    allow uploading of open stream(we use network...)

--- lib/speechmatics/user/jobs.rb
+++ lib/speechmatics/user/jobs.rb
@@ -33,13 +33,13 @@ module Speechmatics
     def attach_audio(params={})
       file_path = params[:data_file]
       raise "No file specified for new job, please provide a :data_file value" unless file_path
-      raise "No file exists at path '#{file_path}'" unless File.exists?(file_path)
+      #raise "No file exists at path '#{file_path}'" unless File.exists?(file_path)
 
-      content_type = params[:content_type] || MimeMagic.by_path(file_path).to_s
-      raise "No content type specified for file, please provide a :content_type value" unless content_type
-      raise "Content type for file '#{file_path}' is not audio or video, it is '#{content_type}'." unless (content_type =~ /audio|video/)
+      content_type = "" # params[:content_type] || MimeMagic.by_path(file_path).to_s
+      # raise "No content type specified for file, please provide a :content_type value" unless content_type
+      # raise "Content type for file '#{file_path}' is not audio or video, it is '#{content_type}'." unless (content_type =~ /audio|video/)
 
-      params[:data_file] = Faraday::UploadIO.new(file_path, content_type)
+      params[:data_file] = Faraday::UploadIO.new(file_path, content_type, params[:original_filename])
       params
     end
 
